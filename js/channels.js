6aca802a62908d8c7297085d3cb81bcc5b818fc6
oreloksana <40369026+oreloksana@users.noreply.github.com>
2020-04-21 12:00:21 +0200

    :cat:

---
+++ js/channels.js
@@ -0,0 +1,29 @@
+const mockChannels = [{
+        id: "0nlbop5f1e",
+        name: "Cat's meeting",
+        favorite: true,
+        messages: [],
+        latestMessage: "17:24"
+    },
+    {
+        id: "6xwd0whqpv",
+        name: "Sweet Party",
+        favorite: true,
+        messages: [],
+        latestMessage: "17:24"
+    },
+    {
+        id: "pc2z0hhkyf",
+        name: "Spring came",
+        favorite: false,
+        messages: [],
+        latestMessage: "17:24"
+    },
+    {
+        id: "achndt7tst",
+        name: "Our slaves",
+        favorite: false,
+        messages: [],
+        latestMessage: "17:24"
+    }
+];
